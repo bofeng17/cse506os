7fb75c5eecf4c81a67ccdaafe077c75e1c919851
arron cao <qqcao.cs@gmail.com>
2015-11-17 01:31:08 -0500

    set gdb logging

--- SBUnix/gdb.sh
+++ SBUnix/gdb.sh
@@ -1,1 +1,1 @@
-gdb -ex 'target remote localhost:9998' -ex 'break boot' -ex 'call boot()' ./kernel
+gdb -ex 'target remote localhost:9998' -ex 'set logging on' -ex 'break boot' -ex 'call boot()' ./kernel

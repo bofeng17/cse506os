e13c013870941e130234a0f3b476893e82eeafb4
bofeng17 <bofeng@cs.stonybrook.edu>
2015-11-18 20:49:25 -0500

    print page fault address

--- SBUnix/sys/idt.c
+++ SBUnix/sys/idt.c
@@ -5,27 +5,27 @@
 #define MAX_IDT 256
 
 idt_entry_t idt[MAX_IDT] =
-  { };
+{ };
 
 static struct idtr_t idtr =
-  { sizeof(idt), (uint64_t) idt, };
+{ sizeof(idt), (uint64_t) idt, };
 
 void
 idt_init ()
 {
-  for (int i = 0; i < MAX_IDT; i++)
+    for (int i = 0; i < MAX_IDT; i++)
     {
-      idt[i].base_lo = 0;
-      idt[i].sel = 0;
-      idt[i].always0 = 0;
-      idt[i].flags = 0;
-      idt[i].base_hi = 0;
-      idt[i].base_64 = 0;
-      idt[i].reserved = 0;
+        idt[i].base_lo = 0;
+        idt[i].sel = 0;
+        idt[i].always0 = 0;
+        idt[i].flags = 0;
+        idt[i].base_hi = 0;
+        idt[i].base_64 = 0;
+        idt[i].reserved = 0;
     }
-  //Remap the irq table.
-  //IRQ 0-15 -> INT 32-47
-  pic_remap ();
+    //Remap the irq table.
+    //IRQ 0-15 -> INT 32-47
+    pic_remap ();
 }
 
 void
@@ -36,16 +36,22 @@ void
 cpu_exception_handler ()
 {
     uint8_t exception_no;
+    uint64_t page_fault_address;
     __asm__ __volatile__("":"=a"(exception_no));
-    printf("CPU exception %x happens!",exception_no);
+    if (exception_no == 0x0E) {
+        __asm__ __volatile__("mov %%cr2, %0":"=r"(page_fault_address));
+        printf("page fault! Address:%p\n",page_fault_address);
+    } else {
+        printf("CPU exception %x happens!\n",exception_no);
+    }
     __asm__ __volatile__("hlt");
 }
 
 void
 reload_idt ()
 {
-  idt_init ();
-  _x86_64_asm_lidt (&idtr);
+    idt_init ();
+    _x86_64_asm_lidt (&idtr);
     //register CPU exception handler exception0
     idt_set_gate (0x00, (uint64_t) exception0, 0x08, 0x8E);
     idt_set_gate (0x01, (uint64_t) exception1, 0x08, 0x8E);
@@ -80,21 +86,21 @@ reload_idt ()
     idt_set_gate (0x1E, (uint64_t) exception30, 0x08, 0x8E);
     idt_set_gate (0x1F, (uint64_t) exception31, 0x08, 0x8E);
     //register timer & kerboard interrupt handler
-  idt_set_gate (0x20, (uint64_t) isr32, 0x08, 0x8E);
-  idt_set_gate (0x21, (uint64_t) isr33, 0x08, 0x8E);
-
+    idt_set_gate (0x20, (uint64_t) isr32, 0x08, 0x8E);
+    idt_set_gate (0x21, (uint64_t) isr33, 0x08, 0x8E);
+    
 }
 
 void
 idt_set_gate (uint8_t num, uint64_t base, uint16_t sel, uint8_t flags)
 {
-  idt[num].base_lo = base & 0xFFFF;
-  idt[num].base_hi = (base >> 16) & 0xFFFF;
-  idt[num].base_64 = (base >> 32) & 0xFFFFFFFF;
-  idt[num].sel = sel;
-  idt[num].always0 = 0;
-  // TODO
-  // We must uncomment the OR below when we get to using user-mode.
-  // It sets the interrupt gate's privilege level to 3.
-  idt[num].flags = flags /* | 0x60 */;
+    idt[num].base_lo = base & 0xFFFF;
+    idt[num].base_hi = (base >> 16) & 0xFFFF;
+    idt[num].base_64 = (base >> 32) & 0xFFFFFFFF;
+    idt[num].sel = sel;
+    idt[num].always0 = 0;
+    // TODO
+    // We must uncomment the OR below when we get to using user-mode.
+    // It sets the interrupt gate's privilege level to 3.
+    idt[num].flags = flags /* | 0x60 */;
 }

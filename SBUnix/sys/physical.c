4a9e81ca16d7b24b3c8e1061e1a2d2a21194d007
junco8 <juncowang716@gmail.com>
2015-11-23 23:21:43 -0500

    add get_page_frame_descriptor

--- SBUnix/sys/physical.c
+++ SBUnix/sys/physical.c
@@ -234,3 +234,10 @@ phy_free (uint64_t addr)
   uint32_t tmp = (addr >> 12);
   set_free (tmp);
 }
+
+page_sp *get_page_frame_descriptor (uint64_t phys_addr)
+{
+  uint32_t index = phys_addr<<12;
+  page_sp* tmp = page_struct_start + index;
+  return tmp;
+}

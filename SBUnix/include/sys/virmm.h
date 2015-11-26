cda92f2630d8969d625d26ac9fb0cd6641da3239
Qingqing Cao <qqcao.cs@gmail.com>
2015-11-25 19:16:40 -0500

    fix set_child_pt cr3

--- SBUnix/include/sys/virmm.h
+++ SBUnix/include/sys/virmm.h
@@ -53,7 +53,7 @@
 #define VMA 5
 #define FILE 6
 
-#define STACK_TOP 0x00000000ffffffff // user stack top
+#define STACK_TOP 0x0000000100000000 // user stack top
 #define STACK_PAGES 0x100 // 512 pages
 #define MAX_HEAP_SIZE (0x1000 * 0x1000)// 4M size
 #define PML4 4

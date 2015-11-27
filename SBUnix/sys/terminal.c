c2f580ef7090420989ce02f2281b6cf16a254b1e
Qingqing Cao <qqcao.cs@gmail.com>
2015-11-26 23:57:32 -0500

    worked!

--- SBUnix/sys/terminal.c
+++ SBUnix/sys/terminal.c
@@ -5,7 +5,7 @@
 // variable and function declarations used by terminal are in sbunix.h
 
 char terminal_buffer[MAX_BUFF];
-int terminal_buf_count; // number of char in the buffer
+int printf_buf_count; // number of char in the buffer
 
 // for terminal write
 int terminal_write(int fd, char *buf, int count) {
@@ -21,18 +21,7 @@ int terminal_read(char *buf, int count){
     
     
     // copy terminal buffer to libc scanf buffer
-    strcpy (buf, terminal_buffer);
-    terminal_buf_count = 0;
+    
     // TODO: how to deal with empty buffer and count?
     return 0;
-}
-
-void terminal_put_char(char c) {
-    if (terminal_buf_count < MAX_BUFF) {
-        terminal_buffer[terminal_buf_count] = c;
-        terminal_buf_count ++;
-    } else {
-        printf("terminal buffer is full!\n");
-        __asm__ __volatile("hlt");
-    }
 }

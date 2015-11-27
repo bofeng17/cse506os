109c63e4d0ed044b2fbe60ee2c3829d2759b75e8
Qingqing Cao <qqcao.cs@gmail.com>
2015-11-26 21:01:36 -0500

    test junco succeed

--- SBUnix/bin/test_malloc/test_malloc.c
+++ SBUnix/bin/test_malloc/test_malloc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/tarfs.h>
 #include <sys/defs.h>
 
 void test(int x, int y, char *s, char a, int d) {
@@ -21,35 +21,42 @@ int data_arr[8192] = { 1, 2, 3, 4 };
 char* s = "hello\n";
 
 int main(int argc, char* argv[], char* envp[]) {
-	//printf("Hello World!\n");
+    //printf("Hello World!\n");
 
-	//    __asm__ __volatile__("INT $0x21;");
-	//        char *ptr_test = (char *)0x200000;
-	//        *ptr_test = 0;
-	//    while (1) {
-	//        ;
-	//    }
-	//   printf("Hello World!\n");
-	int size = 4000;
-	int * t = malloc(size * sizeof(int));
+    //    __asm__ __volatile__("INT $0x21;");
+    //        char *ptr_test = (char *)0x200000;
+    //        *ptr_test = 0;
+    //    while (1) {
+    //        ;
+    //    }
+    //   printf("Hello World!\n");
+    struct dirent* a = malloc(sizeof(struct dirent));
+    printf("dirent size: %d\n", sizeof(struct dirent));
 
-	if (t) {
-		int i = 0;
-		for (i = 0; i < size; i++) {
-			t[i] = i;
-			printf("t[%d] is:%d \n", i, t[i]);
-		}
+    void* b = opendir("bin/");
 
-	}
+    readdir(b, a);
 
-	int x = 1;
-	char y = 'h';
-	test(c[3], data_arr[0], s, y, x);
+    int size = 4000;
+    int * t = malloc(size * sizeof(int));
+
+    if (t) {
+        int i = 0;
+        for (i = 0; i < size; i++) {
+            t[i] = i;
+            printf("t[%d] is:%d \n", i, t[i]);
+        }
+
+    }
+
+    int x = 1;
+    char y = 'h';
+    test(c[3], data_arr[0], s, y, x);
 
 //    int n = 1, fd = 1, count = 5;
 //    char* s = "hello\n";
-	//    __asm__ __volatile__("syscall"
-	//                         :
-	//                         :"a"(n), "D"(fd), "S"((uint64_t)s), "d"(count));
-	return 0;
+    //    __asm__ __volatile__("syscall"
+    //                         :
+    //                         :"a"(n), "D"(fd), "S"((uint64_t)s), "d"(count));
+    return 0;
 }

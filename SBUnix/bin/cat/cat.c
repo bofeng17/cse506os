0859a7f1b39d449a34b6a20278954215c89ad95b
junco8 <juncowang716@gmail.com>
2015-11-27 19:06:04 -0500

    add cat

---
+++ SBUnix/bin/cat/cat.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <syscall.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/tarfs.h>
+
+
+int
+main (int argc, char* argv[], char* envp[])
+{
+	
+
+   char* test_wr=malloc(sizeof(char));
+   struct file* file = open("bin/db.sh", O_RDONLY);
+   read(file, test_wr, 1000);
+   printf("%s\n", test_wr);
+
+   //printf("READDIR cat!!!====== %s\n", test_wr);
+	
+	
+}

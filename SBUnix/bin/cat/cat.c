5e1df39dfc208a2345ac523b3729a95bf121a4ab
junco8 <juncowang716@gmail.com>
2015-11-27 22:56:46 -0500

    add check for cat

--- SBUnix/bin/cat/cat.c
+++ SBUnix/bin/cat/cat.c
@@ -13,6 +13,10 @@ main (int argc, char* argv[], char* envp[])
 
    char* test_wr=malloc(sizeof(char));
    struct file* file = open("bin/db.sh", O_RDONLY);
+    if (file == NULL) {
+   	 printf("no such file exists!!!");
+     return -1;
+     }
    read(file, test_wr, 1000);
    printf("%s\n", test_wr);
 

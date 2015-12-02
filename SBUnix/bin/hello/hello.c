8cdb41533afeaf9eb0ab364fd828557a7946518f
bofeng17 <bofeng@cs.stonybrook.edu>
2015-12-02 00:19:11 -0500

    recover Makefile, add init process

--- SBUnix/bin/hello/hello.c
+++ SBUnix/bin/hello/hello.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[], char* envp[]) {
-    printf("Hello World!\n");
+    int i = 0;
+    int pid = fork();
+    if (pid >0) {
+        while (1) {
+            printf("a %d\n",i);
+            i++;
+        }
+    } else {
+        while (1) {
+            printf("111111111111111 %d\n",i);
+            i++;
+        }
+    }
+    
     return 0;
 }
---
+++ SBUnix/bin/init/init.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[], char* envp[]) {
+    pid_t pid;
+    int status;
+    while (1) {
+        status = 0;
+        // login here
+        printf("init process started: \n");
+        pid = fork();
+        if (pid > 0) {
+            waitpid(pid, &status, 0);
+            if (status == -1) { // process.h: #define ILLEGAL_MEM_ACC 1
+                printf("shell killed by illegal memory access!\n");
+            }
+        } else if (pid == 0) {
+            execve("bin/sbush", argv, envp);
+        } else {
+            printf("error in fork!\n");
+        }
+    }
+    return 0;
+}
